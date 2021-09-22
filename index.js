fe3af27f886f5fdb960c5e625f0cdbf54c1ab365
jlow64 <justin64low@gmail.com>
2021-09-23 01:18:54 +1200

    refactor code structure

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({credentials: true}));
@@ -26,13 +26,13 @@ app.use("/dashboard", require("./routes/dashboard"));
 app.use("/exercise", require("./routes/exercise-route"));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static( 'client/build'));
+    app.use(express.static( 'client/build/'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log("Server has started on port 5000");
 });
