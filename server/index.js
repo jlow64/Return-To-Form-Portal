dce21dca8f0de4ee0e7ff2770d5d89a87641c537
jlow64 <justin64low@gmail.com>
2021-09-23 16:37:47 +1200

    removed azure deployment

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors({origin: ['http://localhost:3000', 'http://192.168.1.79:3000', 'https://return-to-form.azurewebsites.net:3000'], credentials: true}));
+app.use(cors({origin: ['http://localhost:3000', 'http://192.168.1.79:3000' ], credentials: true}));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 app.use(cookieParser());
@@ -24,13 +24,13 @@ app.use("/dashboard", require("./routes/dashboard"));
 
 app.use("/exercise", require("./routes/exercise-route"));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static( 'client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static( 'client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+//     });
+// }
 
 app.listen(port, () => {
     console.log("Server has started on port 5000");
